6889d4e893f78624812a40b392a931d677c877d3
= <=>
2025-05-27 22:16:54 -0300

    feat: adicionando formulário, validação, função de excluir

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,51 @@ import './App.css'
 import { Button, Checkbox, InputAdornment, TextField } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Tarefa } from './components/Tarefa';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import uuid4 from "uuid4";
+
 
 
 
 function App() {
-  const listaTarefas = [{ mensagem: "Teste mensagem 1" }, { mensagem: "Teste mensagem 2" }, { mensagem: "Teste mensagem 3" }]
-  return (
+  const [listaTarefa, setListaTarefa] = useState([])
+  console.log(JSON.stringify(listaTarefa, null, 2));
 
+
+  const excluirTarefa = (id) => {
+    const arrayAtualizado = listaTarefa.filter(item => item.id !== id);
+    setListaTarefa(arrayAtualizado);
+  }
+
+
+
+
+  const formik = useFormik({
+    initialValues: {
+      tituloTarefa: ""
+    },
+
+    onSubmit: values => {
+      setListaTarefa([...listaTarefa, {
+        id: uuid4(),
+        mensagem: values.tituloTarefa,
+        resolvida: false
+      }])
+
+    },
+    validate: values => {
+      const erros = {}
+      if (values.tituloTarefa.length === 0) {
+        erros.tituloTarefa = "Campo obrigatório!"
+      }
+      return erros;
+    }
+  });
+
+  console.log(formik.errors.tituloTarefa)
+  console.log(!!formik.errors.tituloTarefa)
+  return (
 
     <>
       {/* <TextField id="outlined-basic" label="Título da tarefa" variant="outlined" />
@@ -18,18 +56,28 @@ function App() {
         Apagar tarefa
       </Button> */}
 
-      <div className='fundoAzul'>
+      < div className='fundoAzul' >
         <div className='quadradoBranco'>
-          <form action="">
-            <TextField id="outlined-basic" label="Título da tarefa" variant="outlined" size="small" />
-            <Button variant="contained">Add</Button>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              id="tituloTarefa"
+              name="tituloTarefa"
+              label="Título da tarefa"
+              variant="outlined"
+              size="small"
+              onChange={formik.handleChange}
+              value={formik.values.tituloTarefa}
+              error={!!formik.errors.tituloTarefa}
+              helperText={formik.errors.tituloTarefa}
+            />
+            <Button variant="contained" type="submit">Add</Button>
           </form>
           <div className='listaTarefa'>
-            {listaTarefas.map(item => <Tarefa mensagem={item.mensagem} />)}
+            {listaTarefa.map(item => <Tarefa tarefaProp={item} acaoExcluir={excluirTarefa} />)}
           </div>
         </div>
 
-      </div>
+      </div >
     </>
 
 
--- src/components/Tarefa/index.jsx
+++ src/components/Tarefa/index.jsx
@@ -3,10 +3,11 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import './style.css'
 
 const Tarefa = (props) => {
+
     return <div className='caixa'>
-        <p>{props.mensagem}</p>
-        <div className='botaoAzul'>
-            <DeleteOutlineIcon htmlColor='#fff'/>
+        <p>{props.tarefaProp.mensagem}</p>
+        <div className='botaoAzul' onClick={() => props.acaoExcluir(props.tarefaProp.id)}>
+            <DeleteOutlineIcon htmlColor='#fff' />
         </div>
     </div>
 }
